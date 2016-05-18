968f38bec6d94da8a6c72182a2ac4d10a914eaf2
Evgeny Cherkashin <eugeneai@irnok.net>
2016-05-18 19:11:19 +0800

    Loading SVGs into HTML-template.

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var fs = require('fs');
+
+var cache = {};
 
 var app = express();
+// var template='main';
+var template='main-simple';
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+app.engine('handlebars', exphbs({defaultLayout: template}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('web'));
@@ -14,10 +20,28 @@ app.get('/', function (req, res) {
   res.render('test', {name:'Linux!'});
 });
 
+app.get('/svg/:id', function (req, res) {
+  var id=req.params.id;
+  svg=load_svg(id);
+  res.render('svg', {id:id, svg:svg});
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
+function load_svg(name) {
+  if (name in cache) {
+    return cache[name];
+  }
+  var lines=fs.readFileSync('pics/'+name+'.svg').toString().split("\n");
+  lines.shift();
+  lines.shift();
+  lines.shift();
+  var s=lines.join('\n');
+  cache[name]=s;
+  return s;
+};
+
+
 // Works
-
-
